f3cf5cf3a23f3cf7de3a71fb8ebac343895286cc
Roland Sandor Nagy <roland.s.nagy@gmail.com>
2017-04-12 23:24:58 +0200

    refakt

--- Controller.cpp
+++ Controller.cpp
@@ -17,21 +17,8 @@ void Controller::processMessage(char* recvbuf, int recvbuflen, unsigned int iRes
     std::cout << "Processing..." << std::endl;
     std::cout << "msg: " << recvbuf << std::endl << std::endl;
     // Todo...
-	PCHAR *msg;
-
-	msg = (PCHAR *)malloc((iResult + 1)* sizeof(PCHAR *));
-	for (size_t i = 0; i < iResult; i++)
-		memcpy(&msg[i], &recvbuf[i], sizeof(PCHAR *));
-	msg[iResult + 1] = '\0';
-
-	//std::cout << "Message received: " << msg << std::endl;
-
-	std::wstring wsTmp(recvbuf[0], recvbuf[iResult]);
-	std::wstring ws = wsTmp;
 
 	POINT p = adjustPopUp();
-
-
 	//drawStringOnPopUp(ws, iResult, p);
 }
 
@@ -59,24 +46,19 @@ POINT *point = new POINT();
 	HWND Result = NULL;
 	DWORD TId, PId;
 
-	Result = GetFocus();
-	if (!Result)
-	{
-		Wnd = GetForegroundWindow();
-		if (Wnd)
-		{
-			TId = GetWindowThreadProcessId(Wnd, &PId);
-			if (AttachThreadInput(GetCurrentThreadId(), TId, TRUE))
-			{
-				if(GetCaretPos(point) == 0)
-                    return *point;
-                Result = GetFocus();
-				ClientToScreen(Result, point);
-				AttachThreadInput(GetCurrentThreadId(), TId, FALSE);
-			}
-		}
-	}
+    Result = GetFocus();
+    Wnd = GetForegroundWindow();
+	if (Result || !Wnd)
+        return *point;
 
+    TId = GetWindowThreadProcessId(Wnd, &PId);
+    if (   !AttachThreadInput(GetCurrentThreadId(), TId, TRUE)
+        || GetCaretPos(point) == 0)
+        return *point;
+
+    Result = GetFocus();
+    ClientToScreen(Result, point);
+    AttachThreadInput(GetCurrentThreadId(), TId, FALSE);
 	return *point;
 }
 
